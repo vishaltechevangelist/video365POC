67806e992a53828cb49ad1acb1bbb89290e2a420
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2019-07-01 18:30:00 +0530

    abstracting keys & urls

--- APITest.php
+++ APITest.php
@@ -3,9 +3,9 @@ require 'vendor/autoload.php'; // include Composer's autoloader
 
 $api = $argv[1];
 
-$appUrl = "https://flipprime.video365.com";
-$appId = "5fa4f1ef-7e52-42c5-bd3b-af0386003229";
-$appKey = "X0hJOzaqRL1S5BZ/oUOIIEM3XMkpkAhBS+qpgeFybHc=";
+$appUrl = "https://xyz.video365.com";
+$appId = "ppppp-qqqqq-rrrrr-ssss";
+$appKey = "kskskskskks=";
 
 $header = array("AppId:$appId", "ApiKey:$appKey", "content-type:application/json");
 $data = array("pageSize"=>20, "PageNumber"=>1);
@@ -34,9 +34,7 @@ case "PlayerDetails":
 case "Upload":
 	$data = array('BulkVideoList'=>array(0=>
 			array(
-				//'VideoUrl'=>'https://b2ccontents.s3-ap-south-1.amazonaws.com/EOL/Contents/2014050700180710/output.mp4',
-				//'Name'=>'output.mp4',
-				'VideoUrl'=>'https://b2ccontents.s3-ap-south-1.amazonaws.com/EOL/Contents/2014050700180710/output.mp4',
+				'VideoUrl'=>'https://video-file-S3-url/output.mp4',
 				'Name'=>'vishal.mp4',
 				'FileHierarchy'=>'tmpfileuploads-dc5bc651-66cd-4f63-969a-6ac360673d2b//',
 				'FileSize'=>''
@@ -84,12 +82,12 @@ case "Unpublish":
 		array('MediaID'=>18
 		)
 	));
-	$response = getResponse("api/v2/Unpublish", $header, $data, "POST");
+	$response = getResponse("/api/v2/Unpublish", $header, $data, "POST");
 	print_r($response);
 
 }
 
-function getResponse($url, $header = array(), $data = array(), $method = "GET", $appUrl = "https://flipprime.video365.com") {
+function getResponse($url, $header = array(), $data = array(), $method = "GET", $appUrl = "https://xyz.video365.com") {
 	$pest = new PestJSON($appUrl);
 	if ($method == "GET") {
 		$method = "get";
